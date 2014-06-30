2dc84b394c0d7599478ac79f26d59a52fdd31cec
Benjamin Tan <demoneaux@gmail.com>
2014-06-30 20:53:35 +0800

    Update test harness

--- test/index.js
+++ test/index.js
@@ -3,49 +3,44 @@ var jsesc = require('jsesc');
 
 var generate = require('../regjsgen').generate;
 
-var generateTests = [].concat(
-  JSON.parse(fs.readFileSync('test/generate_input.json') || '[]'),
-  JSON.parse(fs.readFileSync('test/generate_unicode_input.json', 'utf8') || '[]')
-);
-var generateResult = [].concat(
-  JSON.parse(fs.readFileSync('test/generate_output.json') || '[]'),
-  JSON.parse(fs.readFileSync('test/generate_unicode_output.json', 'utf8') || '[]')
-);
-
-if (generateTests.length !== generateResult.length) {
-  fail('Generate input and output file needs to have same number of arguments');
-}
-
 var stringify = function(obj) {
   return jsesc(obj, {
-    json: true, compact: true
+    json: true,
+    compact: false,
+    indent: '  '
   });
 };
 
-generateTests.forEach(function(ast, idx) {
-  var generated;
-  try {
-    generated = generate(ast);
-  } catch (error) {
-    var err = error;
-    generated = {
-      type: 'error',
-      name: error.name,
-      message: error.message
-    };
-  }
-
-  var result = generateResult[idx],
-      astString = stringify(ast),
-      generatedStringified = stringify(generated);
-
-  if (generatedStringified !== stringify(result)) {
-    if (err) {
-      console.error(err.stack);
+var runTests = function(data) {
+  Object.keys(data).forEach(function(regex) {
+    var results = data[regex];
+    var generated;
+    try {
+      generated = generate(regex);
+    } catch (exception) {
+      generated = {
+        type: 'error',
+        name: exception.name,
+        message: exception.message,
+        input: regex
+      };
+      var stack = exception.stack;
     }
-    throw new Error('Failure generating ' + stringify(result) +
-      '\nfrom AST:\n' + astString + '\n\n' + generatedStringified);
-  } else {
-    console.log('PASSED TEST: ' + astString);
-  }
-});
+
+    if (stringify(generated) !== stringify(results)) {
+      console.log(
+        'Failure parsing string ' + regex +  ':' + JSON.stringify(generated) +
+        '\n' + JSON.stringify(results)
+      );
+      if (stack) {
+        console.log(stack);
+      }
+      process.exit(1);
+    } else {
+      console.log('PASSED TEST: ' + regex);
+    }
+  });
+};
+
+runTests(require('./test-data.json'));
+runTests(require('./test-data-unicode.json'));
---
+++ test/update_fixtures.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+var fs = require('fs'),
+    path = require('path');
+
+var got = require('got');
+
+function saveFile(url, name, cb) {
+  cb || (cb = function(err) {
+    if (err) {
+      throw err;
+    }
+    console.log('`%s` updated successfully.', name)
+  });
+  got(url, function(err, data) {
+    if (err) {
+      return cb(err);
+    }
+    fs.writeFile(path.join(__dirname, name), data, function(err) {
+      if (err) {
+        return cb(err);
+      }
+      cb();
+    })
+  });
+}
+
+saveFile('https://raw.githubusercontent.com/jviereck/regjsparser/master/test/test-data-unicode.json', 'test-data-unicode.json');
+saveFile('https://raw.githubusercontent.com/jviereck/regjsparser/master/test/test-data.json', 'test-data.json');
